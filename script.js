3d1eda765c78458edfbed5375a89d6fe7361ed5b
gogabarnabishvili <99512619+gogabarnabishvili@users.noreply.github.com>
2022-11-30 03:11:34 +0400

    js exercise done

--- script.js
+++ script.js
@@ -53,15 +53,25 @@ if (Object.keys(stolenItems).length) {
 // 6 is a factor of 12
 // 12 is a factor of 36
 // write JS code that determines whether or not an array is a factor chain.
-const arr1 = [3, 6, 12, 36];
+const arr = [3, 6, 12, 36];
 
-for (let i = 0; i < arr1.length - 1; i++) {
-    if (arr1[i + 1] % arr1[i] === 0) {
-        console.log(true)
+// for (let i = 0; i < arr1.length - 1; i++) {
+//     if (arr1[i + 1] % arr1[i] === 0) {
+//         console.log(true)
+//     } else {
+//         console.log(false)
+//     }
+// }
+
+
+for (var i = 0; i < arr.length - 1; i++) {
+    if (arr[i + 1] % arr[i] === 0) {
+        console.log(true);
     } else {
         console.log(false)
     }
 }
+
 
 // arr1.forEach(isChain);
 // function isChain() {
@@ -80,10 +90,15 @@ for (let i = 0; i < arr1.length - 1; i++) {
 // exe 6
 const arr7 = ["###", "#*#", "###"];
 // write JS code that logs true if an asterisk * is inside a box, and false otherwise.
+
 for (let i = 0; i < arr7.length; i++) {
+
     for (let i2 = 0; i2 < arr7[0].length; i2++) {
-        if (arr7[i][i2] === "*") {
+        const arr10 = arr7[i][i2] === "*";
+        if (arr10) {
             console.log("true")
+
+
         } else {
             console.log("false")
         }
@@ -96,4 +111,15 @@ for (let i = 0; i < arr7.length; i++) {
 const mean7 = 12345;
 // write JS code that logs the mean of all digits.
 // The mean of all digits is the sum of digits / how many digits there are (e.g. mean of digits in 512 is (5+1+2)/3(number of digits) = 8/3=2).
-// The mean will always be an integer.
+// The mean will always be an integer
+
+let mean8 = mean7
+let total = 0;
+let num = 0;
+while (mean8) {
+    total += mean8 % 10;
+    mean8 = Math.floor(mean8 / 10);
+    num += 1;
+}
+
+console.log(Math.floor(total / num));
